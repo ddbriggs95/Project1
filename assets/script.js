441d39a1ad9abc1c4dacfcb49d743e834f8d2228
ParisaDarkhal <111551848+ParisaDarkhal@users.noreply.github.com>
2023-04-18 14:48:02 -0400

    changes

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,15 @@ var searchBar = document.getElementsByClassName(".form-control");
 //variable for getting value user types in search
 var input = document.querySelector('input[type="search"]');
 
-input.addEventListener("click", function(event) {
-    console.log('');
+input.addEventListener("click", function (event) {
+  console.log("");
 });
 
-var requestUrl = 'https://api.api-ninjas.com/v1/dogs?name=';
+var requestUrl = "https://api.api-ninjas.com/v1/dogs?name=";
 
-
-
-
-// fetch(requestUrl)
-// .then(function (response) {
-//     return response.json();
-// })
-// .then(function)
+fetch(requestUrl)
+.then(function (response) {
+    return response.json();
+})
+.then(function)
 
